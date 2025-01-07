bcdb4b472d2fc02912b178f98197c116e3e9dd4e
zackypee <155170165+zackypee@users.noreply.github.com>
2025-01-07 18:18:54 +0100

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,14 @@
+let zackyFavouriteCar = "lambogini urus 2025";
+function purchaseCar(car){
+    if( car === "lambogini urus"){
+        return "purchase the car";
+    } else if ( car === "GLE 450"){
+        return "zacky will manage it";
+    } else {
+        return " zackypee is not interest in buying your car";
+    }
+
+}
+
+console.log(purchaseCar("lambogini urus"))
+
